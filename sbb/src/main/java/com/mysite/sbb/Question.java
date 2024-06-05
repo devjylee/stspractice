0f6e1878b0ae63530494ee019ff3de8fb718f3fa
devjylee <wbs0810@gmail.com>
2024-06-05 17:32:59 +0900

    2-04

---
+++ sbb/src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,42 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+//엔티티로 만들기 위한 애너테이션
+public class Question {
+	
+	@Id
+	//id 속성을 기본키로 지정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//데이터를 저장할 때 해당 속성에 값을 일일이 입력하지 않아도 자동으로 1씩 증가하여 저장
+	private Integer id;
+	
+	@Column(length = 200)
+	//열의 세부설정(길이 설정)
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT")
+	//열의 세부설정(텍스트를 열 데이터로 넣을 수 있다는 의미)
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	//답변 엔티티 참조
+	private List<Answer> answerList;
+
+}
