03c6e3bde8c2db1b953c2e7519402f153737aa79
devjylee <wbs0810@gmail.com>
2024-06-14 17:12:20 +0900

    3-2

--- sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -2,6 +2,9 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //JpaRepository 
@@ -12,5 +15,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	Question findBySubject(String subject);
 	Question findBySubjectAndContent(String subject, String content);
 	List<Question> findBySubjectLike(String subject);
+	Page<Question> findAll(Pageable pageable);
 	
 }
